dfca54ce30c8ef0773470b27cca9ed0d7e1d6dfa
Lourens Veen <l.veen@esciencecenter.nl>
2020-02-17 17:04:35 +0100

    Refactor WorkflowEngine into GlobalWorkflowRunner

--- poc.py
+++ poc.py
@@ -582,77 +582,89 @@ class PolicyManager:
         return rules
 
 
-class WorkflowEngine:
-    """Executes workflows across sites in DDM.
+class WorkflowPlanner:
+    """Plans workflow execution across sites in DDM.
     """
     def __init__(
-            self, policy_manager: PolicyManager, ddm_client: DDMClient
+            self, ddm_client: DDMClient, policy_manager: PolicyManager
             ) -> None:
-        """Create a WorkflowEngine.
+        """Create a GlobalWorkflowRunner.
 
         Args:
             policy_manager: Component that knows about policies.
         """
-        self._policy_manager = policy_manager
         self._ddm_client = ddm_client
+        self._policy_manager = policy_manager
 
-    def execute(
-            self, submitter: str, workflow: Workflow, inputs: Dict[str, str]
-            ) -> Dict[str, Any]:
-        """Plans and executes the given workflow.
+    def make_plans(
+            self, submitter: str,
+            workflow: Workflow, inputs: Dict[str, str]
+            ) -> List[Plan]:
+        """Assigns a site to each workflow step.
+
+        Uses the given result collections to determine where steps can
+        be executed.
 
         Args:
-            submitter: Name of the party to submit this request.
-            workflow: The workflow to execute.
-            inputs: A dictionary mapping the workflow's input
-                    parameters to references to data sets.
+            submitter: Name of the party which submitted this, and to
+                    which results should be returned.
+            workflow: The workflow to plan.
+            result_collections: List of sets of collections
+                    representing access permissions for previously
+                    computed results, indexed by result id.
+
+        Returns:
+            A list of plans, each consisting of a list of sites
+            corresponding to the given list of steps.
         """
-        result_collections = self._find_result_collections(
-                workflow, inputs)
-        print('Result collections:')
-        for result, coll in result_collections.items():
-            print('    {} -> {}'.format(result, coll))
-        print()
+        def may_access_step(
+                result_collections: Dict[str, List[Set[str]]],
+                step: WorkflowStep, party: str
+                ) -> bool:
+            step_key = 'steps.{}'.format(step.name)
+            asset_coll = result_collections[step_key]
+            return self._policy_manager.may_access(asset_coll, party)
 
-        plans = self._make_plans(submitter, workflow, result_collections)
-        print('Plans:')
-        for plan in plans:
-            for step, site in plan.items():
-                print('    {} -> {}'.format(step.name, site))
-            print()
-        print()
+        def may_run(
+                result_collections: Dict[str, List[Set[str]]],
+                step: WorkflowStep, runner: str
+                ) -> bool:
+            """Checks whether the given runner may run the given step.
+            """
+            party = self._ddm_client.get_runner_administrator(runner)
 
-        if not plans:
-            raise RuntimeError(
-                    'This workflow cannot be run due to insufficient'
-                    ' permissions.')
+            # check each input
+            for inp_name in step.inputs:
+                inp_key = 'steps.{}.inputs.{}'.format(step.name, inp_name)
+                asset_coll = result_collections[inp_key]
+                if not self._policy_manager.may_access(asset_coll, party):
+                    return False
 
-        selected_plan = plans[-1]
-        selected_runner_names = set(selected_plan.values())
-        for runner_name in selected_runner_names:
-            self._ddm_client.execute_plan(
-                    runner_name, workflow, inputs, selected_plan)
+            # check step itself (i.e. outputs)
+            return may_access_step(result_collections, step, party)
 
-        # get workflow outputs
-        results = dict()    # type: Dict[str, Any]
-        while len(results) < len(workflow.outputs):
-            for wf_outp_name, wf_outp_source in workflow.outputs.items():
-                if wf_outp_name not in results:
-                    src_step_name, src_step_output = wf_outp_source.split('/')
-                    src_runner_name = selected_plan[workflow.steps[src_step_name]]
-                    src_store = self._ddm_client.get_target_store(
-                            src_runner_name)
-                    outp_key = 'steps.{}.outputs.{}'.format(
-                            src_step_name, src_step_output)
-                    try:
-                        results[wf_outp_name] = self._ddm_client.retrieve_data(
-                                src_store, outp_key)
-                    except KeyError:
-                        continue
-            sleep(0.5)
+        result_collections = self._find_result_collections(workflow, inputs)
+        sorted_steps = self._sort_workflow(workflow)
+        plan = [''] * len(sorted_steps)
 
-        return results
+        def plan_from(cur_step_idx: int) -> Generator[List[str], None, None]:
+            """Make remaining plan, starting at cur_step.
 
+            Yields any complete plans found.
+            """
+            cur_step = sorted_steps[cur_step_idx]
+            step_name = cur_step.name
+            for runner in self._ddm_client.list_runners():
+                if may_run(result_collections, cur_step, runner):
+                    plan[cur_step_idx] = runner
+                    if cur_step_idx == len(plan) - 1:
+                        if may_access_step(
+                                result_collections, cur_step, submitter):
+                            yield copy(plan)
+                    else:
+                        yield from plan_from(cur_step_idx + 1)
+
+        return [dict(zip(sorted_steps, plan)) for plan in plan_from(0)]
 
     def _sort_workflow(self, workflow: Workflow) -> List[WorkflowStep]:
         """Sorts the workflow's steps topologically.
@@ -819,67 +831,102 @@ class WorkflowEngine:
 
         return requirements
 
-    def _make_plans(
-            self, submitter: str, workflow: Workflow,
-            result_collections: Dict[str, List[Set[str]]]
-            ) -> List[Plan]:
-        """Assigns a site to each workflow step.
 
-        Uses the given result collections to determine where steps can
-        be executed.
+class WorkflowExecutor:
+    """Executes workflows across sites in DDM.
+    """
+    def __init__(self, ddm_client: DDMClient) -> None:
+        """Create a WorkflowExecutor.
 
         Args:
-            submitter: Name of the party which submitted this, and to
-                    which results should be returned.
-            workflow: The workflow to plan.
-            result_collections: List of sets of collections
-                    representing access permissions for previously
-                    computed results.
+            ddm_client: A client for connecting to other sites.
+        """
+        self._ddm_client = ddm_client
+
+    def execute_workflow(
+            self, workflow: Workflow, inputs: Dict[str, str], plan: Plan
+            ) -> Dict[str, Any]:
+        """Executes the given workflow execution plan.
+
+        Args:
+            workflow: The workflow to execute.
+            inputs: A dictionary mapping the workflow's input
+                    parameters to references to data sets.
+            plan: The plan according to which to execute the workflow.
 
         Returns:
-            A list of plans, each consisting of a list of sites
-            corresponding to the given list of steps.
+            A dictionary of results, indexed by workflow output name.
         """
-        def may_access_step(step: WorkflowStep, party: str) -> bool:
-            step_key = 'steps.{}'.format(step.name)
-            asset_coll = result_collections[step_key]
-            return self._policy_manager.may_access(asset_coll, party)
+        # launch all the local runners
+        selected_runner_names = set(plan.values())
+        for runner_name in selected_runner_names:
+            self._ddm_client.execute_plan(
+                    runner_name, workflow, inputs, plan)
 
-        def may_run(step: WorkflowStep, runner: str) -> bool:
-            """Checks whether the given runner may run the given step.
-            """
-            party = self._ddm_client.get_runner_administrator(runner)
+        # get workflow outputs whenever they're available
+        results = dict()    # type: Dict[str, Any]
+        while len(results) < len(workflow.outputs):
+            for wf_outp_name, wf_outp_source in workflow.outputs.items():
+                if wf_outp_name not in results:
+                    src_step_name, src_step_output = wf_outp_source.split('/')
+                    src_runner_name = plan[workflow.steps[src_step_name]]
+                    src_store = self._ddm_client.get_target_store(
+                            src_runner_name)
+                    outp_key = 'steps.{}.outputs.{}'.format(
+                            src_step_name, src_step_output)
+                    try:
+                        results[wf_outp_name] = self._ddm_client.retrieve_data(
+                                src_store, outp_key)
+                    except KeyError:
+                        continue
+            sleep(0.5)
 
-            # check each input
-            for inp_name in step.inputs:
-                inp_key = 'steps.{}.inputs.{}'.format(step.name, inp_name)
-                asset_coll = result_collections[inp_key]
-                if not self._policy_manager.may_access(asset_coll, party):
-                    return False
+        return results
 
-            # check step itself (i.e. outputs)
-            return may_access_step(step, party)
 
-        sorted_steps = self._sort_workflow(workflow)
-        plan = [''] * len(sorted_steps)
+class GlobalWorkflowRunner:
+    """Plans and runs workflows across sites in DDM.
+    """
+    def __init__(
+            self, policy_manager: PolicyManager, ddm_client: DDMClient
+            ) -> None:
+        """Create a GlobalWorkflowRunner.
 
-        def plan_from(cur_step_idx: int) -> Generator[List[str], None, None]:
-            """Make remaining plan, starting at cur_step.
+        Args:
+            policy_manager: Component that knows about policies.
+        """
+        self._planner = WorkflowPlanner(ddm_client, policy_manager)
+        self._executor = WorkflowExecutor(ddm_client)
+        self._ddm_client = ddm_client
 
-            Yields any complete plans found.
-            """
-            cur_step = sorted_steps[cur_step_idx]
-            step_name = cur_step.name
-            for runner in self._ddm_client.list_runners():
-                if may_run(cur_step, runner):
-                    plan[cur_step_idx] = runner
-                    if cur_step_idx == len(plan) - 1:
-                        if may_access_step(cur_step, submitter):
-                            yield copy(plan)
-                    else:
-                        yield from plan_from(cur_step_idx + 1)
+    def execute(
+            self, submitter: str, workflow: Workflow, inputs: Dict[str, str]
+            ) -> Dict[str, Any]:
+        """Plans and executes the given workflow.
 
-        return [dict(zip(sorted_steps, plan)) for plan in plan_from(0)]
+        Args:
+            submitter: Name of the party to submit this request.
+            workflow: The workflow to execute.
+            inputs: A dictionary mapping the workflow's input
+                    parameters to references to data sets.
+        """
+        plans = self._planner.make_plans(submitter, workflow, inputs)
+        print('Plans:')
+        for plan in plans:
+            for step, site in plan.items():
+                print('    {} -> {}'.format(step.name, site))
+            print()
+        print()
+
+        if not plans:
+            raise RuntimeError(
+                    'This workflow cannot be run due to insufficient'
+                    ' permissions.')
+
+        selected_plan = plans[-1]
+        results = self._executor.execute_workflow(
+                workflow, inputs, selected_plan)
+        return results
 
 
 class Site:
@@ -913,7 +960,7 @@ class Site:
 
         # Client side
         self._policy_manager = PolicyManager(rules)
-        self._workflow_engine = WorkflowEngine(
+        self._workflow_engine = GlobalWorkflowRunner(
                 self._policy_manager, self._ddm_client)
 
     def __repr__(self) -> str:

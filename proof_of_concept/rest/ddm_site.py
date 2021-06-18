ec36135ac5eb17a9ef6859f7de04809adf7c2933
Lourens Veen <l.veen@esciencecenter.nl>
2021-06-18 13:45:11 +0200

    Set Mahiru log level from the configuration file

--- proof_of_concept/rest/ddm_site.py
+++ proof_of_concept/rest/ddm_site.py
@@ -539,24 +539,30 @@ class Settings:
         namespace: Namespace controlled by the site's policy server.
         owner: Party owning the site.
         registry_endpoint: Registry endpoint location.
+        loglevel: Logging level to use, one of 'critical', 'error',
+                'warning', 'info', or 'debug'.
     """
     def __init__(
             self,
             name: str, namespace: str, owner: Identifier,
-            registry_endpoint: str
+            registry_endpoint: str, loglevel: str = 'info'
             ) -> None:
         """Create a Settings object.
 
         Args:
-            name: Name of the site.
+            name: Name of the site (without namespace or tag).
             namespace: Namespace controlled by the site's policy server.
-            owner: Party owning the site.
+            owner: Id of the party owning the site, e.g.
+                    "party:namespace:name".
             registry_endpoint: Registry endpoint location.
+            loglevel: Logging level to use, one of 'critical', 'error',
+                    'warning', 'info', or 'debug'.
         """
         self.name = name
         self.namespace = namespace
         self.owner = owner
         self.registry_endpoint = registry_endpoint
+        self.loglevel = loglevel
 
 
 load_settings = yatiml.load_function(Settings, Identifier)
@@ -568,6 +574,8 @@ default_config_location = Path('/etc/mahiru/mahiru.conf')
 def wsgi_app() -> App:
     """Creates a WSGI app for a WSGI runner."""
     settings = load_settings(default_config_location)
+
+    logging.basicConfig(level=settings.loglevel.upper())
 
     registry_rest_client = RegistryRestClient(settings.registry_endpoint)
     registry_client = RegistryClient(registry_rest_client)
